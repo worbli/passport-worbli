688e074b5b365e5eba4265134db19282218824c3
Ivaylo Ushev <iushev@yahoo.com>
2019-10-30 09:47:04 +0200

    Update parse user's data

--- test/profile.test.js
+++ test/profile.test.js
@@ -4,26 +4,38 @@ const Profile = require('../lib/profile')
 
 describe('Profile.parse', function() {
 
-  describe('profile obtained from Users documentation on 2016/02/02', function() {
+  describe('profile obtained from user with all data', function() {
     var profile;
 
     before(function(done) {
       fs.readFile('test/fixtures/user.json', 'utf8', function(err, data) {
         if (err) { return done(err); }
-        // profile = Profile.parse(data);
+        profile = Profile.parse(data);
         done();
       });
     });
 
     it('should parse profile', function() {
-      // expect(profile.id).to.equal('1');
-      // expect(profile.username).to.equal('octocat');
-      // expect(profile.displayName).to.equal('monalisa octocat');
-      // expect(profile.profileUrl).to.equal('https://github.com/octocat');
-      // expect(profile.emails).to.have.length(1);
-      // expect(profile.emails[0].value).to.equal('octocat@github.com');
-      // expect(profile.photos).to.have.length(1);
-      // expect(profile.photos[0].value).to.equal('https://github.com/images/error/octocat_happy.gif');
+      expect(profile.id).to.equal('1');
+      expect(profile.displayName).to.equal('Bill Jone Topper');
+      expect(profile.emails).to.have.length(1);
+      expect(profile.emails[0].value).to.equal('user@example.com');
+    });
+  });
+
+  describe('profile obtained from user with partial data', function() {
+    var profile;
+
+    before(function(done) {
+      fs.readFile('test/fixtures/user-partial.json', 'utf8', function(err, data) {
+        if (err) { return done(err); }
+        profile = Profile.parse(data);
+        done();
+      });
+    });
+
+    it('should parse profile', function() {
+      expect(profile.id).to.equal('1');
     });
   });
 });
